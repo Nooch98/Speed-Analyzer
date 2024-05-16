21450e70b7b1ce0bf92a55fdd2fb30377262311c
Nooch98 <73700510+Nooch98@users.noreply.github.com>
2024-05-16 23:32:46 +0200

    Update speed_analyzer.bat

--- speed_analyzer.bat
+++ speed_analyzer.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File " C:\Users\Nooch\Documents\PowerShell\Scripts\speed_analyzer.ps1"
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File " $HOME\Documents\PowerShell\Scripts\speed_analyzer.ps1"
